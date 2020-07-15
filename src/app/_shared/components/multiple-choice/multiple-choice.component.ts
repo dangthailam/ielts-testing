1b2bcf997804012469f7c6f6f720f71d5c7c7b2b
DANG Thai lam <lamtp1989@gmail.com>
2020-07-15 09:57:36 +0200

    add lodash + multiple choice component

--- src/app/_shared/components/multiple-choice/multiple-choice.component.ts
+++ src/app/_shared/components/multiple-choice/multiple-choice.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import * as InlineEditor from '@ckeditor/ckeditor5-build-inline';
 import { Question } from '../../models/question.model';
 import { QuestionType } from '../../models/questionType.enum';
-import { faPlus, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faEdit, faCheck, faBan } from '@fortawesome/free-solid-svg-icons';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'app-multiple-choice',
@@ -16,17 +17,36 @@ export class MultipleChoiceComponent implements OnInit {
 
     faPlus = faPlus;
     faEdit = faEdit;
+    faCheck = faCheck;
+    faBan = faBan;
     Editor = InlineEditor;
+    onEdit = false;
+    private oldMultipleChoice: Question;
     constructor() { }
 
 
     ngOnInit(): void {
         this.multipleChoice = this.multipleChoice || new Question(QuestionType.MultipleChoice);
         this.multipleChoice.answers = [
-            "<p>Option 1</p>",
-            "<p>Option 2</p>",
-            "<p>Option 3</p>"
+            '<p>Option 1</p>',
+            '<p>Option 2</p>',
+            '<p>Option 3</p>'
         ]
     }
 
+    editQuestion() {
+        this.oldMultipleChoice = _.cloneDeep(this.multipleChoice);
+        this.onEdit = true;
+    }
+
+    cancel() {
+        this.onEdit = false;
+        this.multipleChoice = this.oldMultipleChoice;
+        this.oldMultipleChoice = null;
+    }
+
+    approve() {
+        this.onEdit = false;
+        this.oldMultipleChoice = null;
+    }
 }
